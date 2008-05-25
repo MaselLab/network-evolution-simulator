5cdcd00c6e88eff0dfd6b27dddd85750baf4e6ef
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2008-05-25 10:50:03 +0000

    Compute averages of protein concentrations (model output) for comparison when random number sequences change.

---
+++ compute-averages.R
@@ -0,0 +1,37 @@
+library(lattice)
+
+if (FALSE) {
+
+  for (i in seq(00, 99)) {
+    cmd=sprintf("../netsim -r %i  -d output%02d", i, i)
+    print(cmd)
+    system(cmd)
+  }
+}
+
+df = NULL
+
+for (i in seq(0,9)) {
+  fs=paste(dir(pattern="output[0-9]+",full.names=T),"/protein",i,".dat",sep="")
+  ##print(fs)
+  ##print(i)
+  for (f in fs) {
+    series=read.table(f)
+    names(series)=c("time", "conc")
+
+    ## get the average of the length of timesteps
+    time.av = mean(diff(series$time))
+
+    ## get the mean protein concentration over the time series
+    conc.av = mean(series$conc)
+
+    ## get the last protein concentration
+    conc.last = tail(series,n=1)$conc
+    ##print(time.av)
+    ##print(conc.av)
+    df <- rbind(df, data.frame(gene=i, time.av=time.av, conc.av=conc.av, conc.last=conc.last))
+  }
+}
+print(df)
+
+xyplot(gene~conc.av, data=df, scales=list(x=list(log=TRUE)))
