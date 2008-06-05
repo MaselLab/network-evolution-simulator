f84eb5520f5cf5c1e689b32757946cc78d1324ce
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2008-06-05 23:19:54 +0000

    Add rowCount, colCount to data structures to keep track of total rows rather than using NULL pointers.  Still needs some cleanup.

--- juribe/OMG.c
+++ juribe/OMG.c
@@ -12,19 +12,20 @@ static int Tmatrix[4][4]={{1, 2, 0, 0}, {4, 5, 0, 6}, {7, 0, 0, 9},
                             
 static double Xvector[4] = {0, 1, 1, 1};
 
-struct Rowtype{
+struct Rowtype {
     int rownum;
     float kval;
 };
     
-struct Ttype{
-    int col;
-    struct Rowtype **row;//TFBS+1 is the maximum # of non zero entries in any column
-    //does the row have to be a pointer?? or a double pointer **??
+struct Ttype {
+  int col;
+  struct Rowtype *row;//TFBS+1 is the maximum # of non zero entries in any column
+  //does the row have to be a pointer?? or a double pointer **??
+  int rowCount;
 };  
          
 
-void compressT(int matrixT[DIM][DIM], struct Ttype **SmallT)
+void compressT(int matrixT[DIM][DIM], struct Ttype *SmallT, int *colCount)
 {
     int i, j, k, m, check, zeros;
     
@@ -61,44 +62,44 @@ void compressT(int matrixT[DIM][DIM], struct Ttype **SmallT)
        printf("k: %d\n", k); 
         
        printf("before SmallT[k] malloc\n");
-       system("PAUSE");
-       SmallT[k]->row=malloc((zeros+1)*sizeof(struct Rowtype));
+       //system("PAUSE");
       
-       SmallT[k]->col=i;
+       SmallT[k].col=i;
+       SmallT[k].row=malloc((zeros+1)*sizeof(struct Rowtype));
 
        m=0;  //m moves through the Rowtype array inside the Ttype structure
        for(j=0; j<pow(2,TFBS); j++) { //j moves through the rows of the T array
            printf("i=%d, j=%d, Tmatrix[j][i]=%d, k=%d, m=%d\n", i, j, Tmatrix[j][i], k, m);
            if(Tmatrix[j][i] != 0) {
-              system("PAUSE");
-              SmallT[k]->row[m]->rownum = j;  //put in row number
-              SmallT[k]->row[m]->kval=Tmatrix[j][i];  //put in value at that place
+	     //system("PAUSE");
+              SmallT[k].row[m].rownum = j;  //put in row number
+              SmallT[k].row[m].kval=Tmatrix[j][i];  //put in value at that place
               printf("after row[m] assignment, k=%d, m=%d, smallT row=%d, kval=%g\n", 
-                k, m, SmallT[k]->row[m]->rownum, SmallT[k]->row[m]->kval);
+                k, m, SmallT[k].row[m].rownum, SmallT[k].row[m].kval);
               m++;  //go to next element in Rowtype array
              
-              system("PAUSE");
+              //system("PAUSE");
            }
        }
        printf("before NULL ptr assignment\n");
-       system("PAUSE");
-       //SmallT[k]->row[m]= NULL;
+       //system("PAUSE");
        printf("after NULL ptr assignment\n");
        //Need to assign NULL pointer at the end of the Rowtype array... not sure how to do this
-       //SmallT[k]->row[m]=NULL;
+       SmallT[k].rowCount = m;
        
        k++;  //if the entire column is non-zero, then go to next place in Ttype array
        i++;
        printf("SmallT, i = %d, k = %d\n", i, k);
     }
-    SmallT[k] = NULL; 
+    //SmallT[k] = NULL; 
+    *colCount = k;
     printf("k: %d\n", k); 
     printf("m: %d\n", m);
     printf("\n");
  
 } 
  
-void multiplyT(struct Ttype **SmallT, float *newX)
+void multiplyT(struct Ttype *SmallT, float *newX, int colCount)
 {  
     int c, n, j;  
     float sum;
@@ -110,20 +111,25 @@ void multiplyT(struct Ttype **SmallT, float *newX)
     }
     
    c=0;  //goes through Tarray
-   while (SmallT[c]!=NULL){  /*goes through Tarray of structures (outer)*/
-       sum=0;  // next part goes through Rowtype array
-       for(n=0; n<3; n++){  /* number of non-zero elements in each column, should be NULL while*/
-           sum+= storeX[(SmallT[c]->row[n]->rownum)]* SmallT[c]->row[n]->kval;/*multiplies element in storeX with corresponding element in SmallT then adds*/
-       }
-       printf("%d %.2f\n", (SmallT[c]->col), sum);
-       newX[(SmallT[c]->col)]=sum;  //store sum of each column in corresponding newX place
-       c++;
-   } 
+   //while (SmallT[c]!=NULL){  /*goes through Tarray of structures (outer)*/
+   while (c < colCount) {
+     sum=0;  // next part goes through Rowtype array
+     //for(n=0; n<3; n++){  /* number of non-zero elements in each column, should be NULL while*/
+     n = 0;
+     while (n < SmallT[c].rowCount) {
+       sum+= storeX[(SmallT[c].row[n].rownum)]* SmallT[c].row[n].kval;/*multiplies element in storeX with corresponding element in SmallT then adds*/
+       n++;
+     }
+   printf("%d %.2f\n", (SmallT[c].col), sum);
+   newX[(SmallT[c].col)]=sum;  //store sum of each column in corresponding newX place
+   c++;
+} 
 }    
 
 int main(){
     int d;
-    struct Ttype **SmallT;
+    int colCount;
+    struct Ttype *SmallT;
     float *newX;
     newX=calloc(4, sizeof(float));
     
@@ -134,38 +140,38 @@ int main(){
 
     SmallT=malloc((pow(2,TFBS)+1)*sizeof(struct Ttype));//=4 * sizeof()
 
-    compressT(Tmatrix, SmallT);
+    compressT(Tmatrix, SmallT, &colCount);
    
     int p,q;
     p=0;
-    while (SmallT[p]!=NULL){
-        printf( "Column: %d\n", SmallT[p]->col); 
-        for(q=0;q<3;q++){
-           printf( "Row%d: %d\n",q, SmallT[p]->row[q]->rownum);
-           printf( "Value%d: %.2f\n",q, SmallT[p]->row[q]->kval);     
-        }
-        p++;    
+    while (p < colCount) {
+      //printf( "Column: %d\n", SmallT[p].col); 
+      while(q < SmallT[p].rowCount) {
+	printf( "Row%d: %d\n",q, SmallT[p].row[q].rownum);
+	printf( "Value%d: %.2f\n",q, SmallT[p].row[q].kval);     
+	q++;
+      }
+      p++;    
     }
-  // printf("Here again"); 
-  // printf("\n");
-   multiplyT(SmallT, newX);
+    // printf("Here again"); 
+    // printf("\n");
+    multiplyT(SmallT, newX, colCount);
    
-   int f;
+    int f;
    for(f=0; f<4; f++){
        printf("%.2f ", newX[f]); 
    } 
-  /* printf("\n"); 
-   multiplyT(SmallT, newX);
+   /* printf("\n"); 
+      multiplyT(SmallT, newX);
+      
+      for(f=0; f<4; f++){
+      printf("%.2f ", newX[f]); 
+      } 
+      printf("\n"); */
    
-   for(f=0; f<4; f++){
-       printf("%.2f ", newX[f]); 
-   } 
-   printf("\n"); */
-   
-
     free(SmallT);
     free(newX);
-    
                                          
-    system("PAUSE"); }
+    //system("PAUSE"); 
+}
       
