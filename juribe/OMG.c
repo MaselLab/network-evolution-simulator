8a3ebe4cc35a9f003f589901c269e3cd00d9a82b
Jasmin Uribe <juribe@email.arizona.edu>
2008-06-10 15:38:47 +0000

    Tightened up compressT code. Other minor changes.

--- juribe/OMG.c
+++ juribe/OMG.c
@@ -5,7 +5,7 @@
 #define TFBS 2
 #define DIM 4
 
-static float Tmatrix[4][4]={{1, 2, 3, 0}, {4, 5, 0, 0}, {7, 0, 8, 0}, {0, 0, 0, 0}};                            
+static float Tmatrix[4][4]={{1, 2, 3, 0}, {4, 5, 0, 6}, {7, 0, 8, 9}, {0, 10, 11, 12}};                            
 static float Xvector[4] = {0, 1, 1, 1};
 
 static float Fmatrix[8][8]={{.2, .2, .1, 0, .4, 0, 0, 0}, {.1, .2, 0, .3, 0, .2, 0, 0}, {.2, 0, .2, .1, 0, 0, .3, 0}, {0, .2, .2, .1, 0, 0, 0, .2},
@@ -27,7 +27,7 @@ struct Ttype {
 
 void compressT(float matrixT[DIM][DIM], struct Ttype *arrayT, int *colCount)
 {
-    int i, j, n, m, check, zeros;
+    int i, j, n, m, check, nonzeros;
      
      for(i=0; i<DIM; i++){  //this just prints the original T matrix
          for(j=0; j<DIM; j++){
@@ -40,36 +40,31 @@ void compressT(float matrixT[DIM][DIM], struct Ttype *arrayT, int *colCount)
     n=0;  //n, m move through the Ttype array and Rowtype array
     i=0;  //i, j move through T matrix
     while (i<pow(2,TFBS)) {
-        zeros=0;
+        nonzeros=0;
         for (check=0; check<pow(2,TFBS); check++) {
             if(matrixT[check][i] != 0)
-                zeros++;
-        }
-        while(zeros == 0){
-           i++;
-           for(check=0; check<pow(2,TFBS); check++) {
-              if(matrixT[check][i] != 0)
-                zeros++;
-           }
-        }                
-       if(i<pow(2,TFBS)){
-         arrayT[n].col=i;
-         arrayT[n].row = malloc((zeros+1)*sizeof(struct Rowtype));
+                nonzeros++;
+        } 
+        if (nonzeros == 0) {
+            i++;             
+        } else {
+            arrayT[n].col = i;
+            arrayT[n].row = malloc((nonzeros+1)*sizeof(struct Rowtype));
 
-         m=0;  //m moves through the Rowtype array inside the Ttype structure
-         for(j=0; j<pow(2,TFBS); j++) { //j moves through the rows of the T array
-             if(matrixT[j][i] != 0) {
-                arrayT[n].row[m].rownum = j;  //put in row number
-                arrayT[n].row[m].kval = matrixT[j][i];  //put in value at that place
-                m++;  //go to next element in Rowtype array
-             }
-         }
-         arrayT[n].rowCount = m;
-         n++;  //if the entire column is non-zero, then go to next place in Ttype array
-         i++;
-       }
-   }    
-   *colCount = n;
+            m=0;  //m moves through the Rowtype array inside the Ttype structure
+            for (j=0; j<pow(2,TFBS); j++) { //j moves through the rows of the T array
+                if (matrixT[j][i] != 0) {
+                   arrayT[n].row[m].rownum = j;  //put in row number
+                   arrayT[n].row[m].kval = matrixT[j][i];  //put in value at that place
+                   m++;  //go to next element in Rowtype array
+                }
+            }
+            arrayT[n].rowCount = m;
+            n++;  //if the entire column is non-zero, then go to next place in Ttype array
+            i++;
+          }
+     }    
+   *colCount = n;   
 } 
  
 void multiplyT(struct Ttype *arrayT, float *newX, int colCount)
@@ -78,7 +73,7 @@ void multiplyT(struct Ttype *arrayT, float *newX, int colCount)
     float sum;
     float storeX[DIM];
    
-    for(j=0; j<DIM; j++){  //copy and store newX array and initialize newX array to 0
+    for (j=0; j<DIM; j++) {  //copy and store newX array and initialize newX array to 0
         storeX[j] = newX[j];
         newX[j] = 0;
     }
@@ -97,25 +92,23 @@ void multiplyT(struct Ttype *arrayT, float *newX, int colCount)
 }    
 
 int main(){
-    int d;
-    int colCount;
+    int d, p, q, colCount;
     struct Ttype *arrayT;
     float *newX;
     newX = calloc(pow(2,TFBS), sizeof(float));
     arrayT = malloc((pow(2,TFBS)+1)*sizeof(struct Ttype));//=4 * sizeof()
     
-    for(d=0; d<pow(2,TFBS); d++){
+    for (d=0; d<pow(2,TFBS); d++) {
          newX[d] = Xvector[d];
      }    
     
     compressT(Tmatrix, arrayT, &colCount);
-   
-    int p,q;
+  
     p=0;
     while (p < colCount) {
        printf( "Column: %d\n", arrayT[p].col); 
        q=0;
-       while(q < arrayT[p].rowCount) {
+       while (q < arrayT[p].rowCount) {
     	  printf( "Row%d: %d\n",q, arrayT[p].row[q].rownum);
 	      printf( "Value%d: %.2f\n",q, arrayT[p].row[q].kval);     
     	  q++;
@@ -126,23 +119,23 @@ int main(){
     printf("\n");
     
     multiplyT(arrayT, newX, colCount);
-    int f;
-    for(f=0; f<pow(2,TFBS); f++){
-        printf("%.2f ", newX[f]); 
+ 
+    for (d=0; d<pow(2,TFBS); d++) {
+        printf("%.2f ", newX[d]); 
     }
     printf("\n"); 
     
     multiplyT(arrayT, newX, colCount);
     multiplyT(arrayT, newX, colCount);
     
-    for(f=0; f<pow(2,TFBS); f++){
-        printf("%.2f ", newX[f]); 
+    for (d=0; d<pow(2,TFBS); d++) {
+        printf("%.2f ", newX[d]); 
     }
     printf("\n");  
    
-    int j;
-    for (j=0; j<colCount; j++){
-        free(arrayT[j].row);
+  
+    for (d=0; d<colCount; d++){
+        free(arrayT[d].row);
     }    
     free(arrayT);
     free(newX);
