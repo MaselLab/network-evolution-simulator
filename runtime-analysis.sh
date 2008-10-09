0d1483f6d7f20ece36312eac28b34b10710526ea
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2008-10-09 12:01:53 +0000

    Add header, loop over kon = 1e-4, 5e-4, 1e-3 ; 6 different genotypes, each done 4 times.

--- runtime-analysis.sh
+++ runtime-analysis.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 outfile=$1
-for kon in 0.0001 0.0005
+# print out header
+echo "kon rand replicate real user sys" > $outfile
+for kon in 0.0001 0.0005 0.001
 do 
-    for rand in 4 5
+    for rand in 4 5 6 7 8 9 10
     do
-	echo -n "$kon $rand " >> $outfile
-	/usr/bin/time -f "%E %U %S" -a -o $outfile ./netsim-selection -r 4 -p 2 -d selection -c -1.0 --kon $kon
+	for replicate in 1 2 3 4
+	do
+	    echo -n "$kon $rand $replicate " >> $outfile
+	    /usr/bin/time -f "%E %U %S" -a -o $outfile ./netsim-selection -r 4 -p 2 -d selection -c -1.0 --kon $kon
+	done
     done
 done
