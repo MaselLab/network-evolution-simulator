005b1956b0e0fd466b27b64a0c5ca7bf2b9d3313
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2008-10-14 04:54:48 +0000

    Loop over 100 random seeds, each doing only one replicate.

--- runtime-analysis.sh
+++ runtime-analysis.sh
@@ -4,9 +4,10 @@ outfile=$1
 echo "kon rand replicate real user sys" > $outfile
 for kon in 0.0001 0.0005 0.001
 do 
-    for rand in 4 5 6 7 8 9 10
+
+    for ((rand=4;rand<104;rand+=1))
     do
-	for replicate in 1 2 3 4
+	for replicate in 1
 	do
 	    echo -n "$kon $rand $replicate " >> $outfile
 	    /usr/bin/time -f "%E %U %S" -a -o $outfile ./netsim-selection -r $rand -p 2 -d selection -c -1.0 --kon $kon
