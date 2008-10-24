33f5ef53821862d311ee57093d6353b112536bd9
Alex Lancaster <alex_lancaster@hms.harvard.edu>
2008-10-24 12:55:01 +0000

    Formatting figure.

--- plot-runtime-analysis.R
+++ plot-runtime-analysis.R
@@ -2,10 +2,20 @@ library(lattice)
 runtimeall=read.table("/tmp/new-runtime/runtime-all.txt", head=T)
 outall=aggregate(runtimeall, by=list(runtimeall$kon), mean)
 outall=transform(subset(outall, kon>1e-10), gen=77040000/(500*real*33))
+
+#png("generations-vs-kon.png")
 postscript("generations-vs-kon.ps")
+
+myat=10^seq(-10,10)
+mylabels=seq(-10,10)
+
 xyplot(gen~kon,
-       scales=list(y=list(log=T),x=list(log=T)),
        data=outall,
+       scales=list(log=TRUE, cex=2.0,
+         x=list(at=myat,labels=mylabels),
+         y=list(at=myat,labels=mylabels)),
+       xlab=list(expression(log~~italic(k[on])), cex=2.0),
+       ylab=list(expression(log~~italic(generations)), cex=2.0),
        panel=function(x, y,...) {
          panel.xyplot(x, y, ...)
          sbs=subset(outall, kon>1e-8)
