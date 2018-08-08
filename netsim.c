b6848459d5440c9f6fb3be8c29a2040493c8fc85
kunxiong <8291347+kunxiong@users.noreply.github.com>
2018-08-08 12:29:28 -0700

    Add licence declaration

--- netsim.c
+++ netsim.c
@@ -1,11 +1,21 @@
 /* 
- * Simulator of yeast transcriptional regulatory network evolution
- * 
  * This file contains functions to initialize simulation with specified 
  * selection condition, and to output summary of genotypes and network structure.
  * 
  * Authors: Joanna Masel, Alex Lancaster, Kun Xiong
- * Copyright (c) 2007-2018 Arizona Board of Regents (University of Arizona)
+ * Copyright (c) 2018 Arizona Board of Regents on behalf of the University of Arizona
+ 
+ * This file is part of network-evolution-simulator.
+ * network-evolution-simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * network-evolution-simulator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with network-evolution-simulator. If not, see <https://www.gnu.org/licenses/>.
  */
 #include <stdio.h>
 #include <stdlib.h>
