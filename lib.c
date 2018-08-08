fb4c864941e1b3a7e1088cef2a78259168c0ab87
kunxiong <8291347+kunxiong@users.noreply.github.com>
2018-08-08 12:26:29 -0700

    Add licence declaration

--- lib.c
+++ lib.c
@@ -1,12 +1,22 @@
 /* 
- * Simulator of yeast transcriptional regulatory network evolution
- *
  * This file mainly contains helper/support functions which aren't
  * necessarily specific to the model, such as
  * maintaining the linked list data structures or file I/O
  *
  * Authors: Joanna Masel, Alex Lancaster, Kun Xiong
- * Copyright (c) 2007-2018 Arizona Board of Regents (University of Arizona)
+ * Copyright (c) 2018 Arizona Board of Regents on behalf of the University of Arizona
+ 
+ * This file is part of network-evolution-simulator.
+ * network-evolution-simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * network-evolution-simulator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with network-evolution-simulator. If not, see <https://www.gnu.org/licenses/>.
  */
 #include <stdlib.h>
 #include <stdio.h>
