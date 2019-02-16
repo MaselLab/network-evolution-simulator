667b604273a7ce3d6e8de517f429f0fae1f4d86b
kunxiong <kunxiong@email.arizona.edu>
2019-02-15 17:15:55 -0700

    Updated post-process scripts

--- post-process_scripts/calc_motif_occurrence.m
+++ post-process_scripts/calc_motif_occurrence.m
@@ -1,7 +1,7 @@
 function motif_occurrence=calc_motif_occurrence(window_size)  
 %%load data
 data=load('N_motifs.txt'); 
-motif_numbers=data(end-window_size+1:end,1:39); 
+motif_numbers=data(end-window_size+1:end,1:36); 
 %%check for presence of a motif
 motif_presence=motif_numbers>0;    
 %%calculate occurrence
